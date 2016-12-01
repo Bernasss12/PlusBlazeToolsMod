0ba463893e1f134922c47249e170c4210688cc60
Bernasss12 <bernasss12@gmail.com>
2016-12-01 16:56:14 +0000

    Updating forge.

--- src/main/java/com/bernasss12/pbtmod/block/ModBlock.java
+++ src/main/java/com/bernasss12/pbtmod/block/ModBlock.java
@@ -1,7 +1,15 @@
 package com.bernasss12.pbtmod.block;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.material.MapColor;
+import net.minecraft.block.material.Material;
+
 /**
  * Created by Bernardo on 30/11/2016.
  */
-public class ModBlock {
+public class ModBlock extends Block
+{
+    public ModBlock(Material blockMaterialIn, MapColor blockMapColorIn) {
+        super(blockMaterialIn, blockMapColorIn);
+    }
 }
--- src/main/java/com/bernasss12/pbtmod/item/ModPickaxe.java
+++ src/main/java/com/bernasss12/pbtmod/item/ModPickaxe.java
@@ -4,21 +4,19 @@ import com.bernasss12.pbtmod.PlusBlazeToolsMain;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.item.EntityItem;
-import net.minecraft.entity.item.EntityXPOrb;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.init.SoundEvents;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.crafting.FurnaceRecipes;
-import net.minecraft.util.EnumActionResult;
-import net.minecraft.util.EnumFacing;
-import net.minecraft.util.EnumHand;
+import net.minecraft.util.*;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import net.minecraftforge.oredict.OreDictionary;
+import net.minecraftforge.client.event.sound.SoundEvent;
 
-import java.util.Map;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Bernardo on 27/11/2016.
@@ -29,41 +27,55 @@ public class ModPickaxe extends ItemPickaxe {
         setUnlocalizedName(name);
         setCreativeTab(PlusBlazeToolsMain.tabGeneral);
     }
-    /**
-    @Override
+
+    /*@Override
     public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
-        if (!worldIn.isRemote) {
-            if (worldIn.getBlockState(pos).getBlock() == Blocks.IRON_ORE) {
-                int x = pos.getX();
-                int y = pos.getY();
-                int z = pos.getZ();
-                worldIn.destroyBlock(pos, false);
-                EntityItem item = new EntityItem(worldIn, x, y, z, new ItemStack(Items.IRON_INGOT, 1));
-                worldIn.spawnEntityInWorld(item);
-                worldIn.spawnEntityInWorld(new EntityXPOrb(worldIn, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, 5));
-                return true;
+        Random r = new Random();
+        int i = r.nextInt(10);
+        int x = pos.getX();
+        int y = pos.getY();
+        int z = pos.getZ();
+
+        if(!worldIn.isRemote) {
+            if (worldIn.getBlockState(pos).getBlock() == (Blocks.IRON_ORE)) {
+                if (i == 5) {
+                    EntityItem item = new EntityItem(worldIn, x, y, z, new ItemStack(Items.IRON_INGOT, 1));
+                    worldIn.destroyBlock(pos, false);
+                    worldIn.spawnEntityInWorld(item);
+                    for(int j = 0; j > 5; j++) {
+                        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + ((double) i * 0.1D), (double) y + ((double) i * 0.1D), (double) z + ((double) i * 0.1D), 0.01D, 0.01D, 0.01D);
+                    }
+                    worldIn.playSound((double) x, (double) y, (double) z, SoundEvents.ENTITY_BLAZE_SHOOT, SoundCategory.BLOCKS, 3.0F + r.nextFloat(), 1.0F, true);
+                }
+            } else if (worldIn.getBlockState(pos).getBlock() == Blocks.GOLD_ORE) {
+                if (i == 5) {
+                    EntityItem item = new EntityItem(worldIn, x, y, z, new ItemStack(Items.GOLD_INGOT, 1));
+                    worldIn.spawnEntityInWorld(item);
+                    worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + ((double) i * 0.1D), (double) y + ((double) i * 0.1D), (double) z + ((double) i * 0.1D), 0.01D, 0.01D, 0.01D);
+                    worldIn.playSound((double) x, (double) y, (double) z, SoundEvents.ENTITY_BLAZE_SHOOT, SoundCategory.BLOCKS, 1.0F + r.nextFloat(), 1.0F, true);
+                }
             }
         }
-        return super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
-    }
-    */
-    @Override
-    public boolean onBlockDestroyed(ItemStack stack, World worldIn, IBlockState state, BlockPos pos, EntityLivingBase entityLiving) {
-        if (!worldIn.isRemote) {
-                int x = pos.getX();
-                int y = pos.getY();
-                int z = pos.getZ();
-                ItemStack block = new ItemStack(worldIn.getBlockState(pos).getBlock(), 1);
-                int exp = (int)FurnaceRecipes.instance().getSmeltingExperience(block);
-                ItemStack result = FurnaceRecipes.instance().getSmeltingResult(block);
-                //worldIn.destroyBlock(pos, false);
-                EntityItem item = new EntityItem(worldIn, x, y, z, result);
-                worldIn.spawnEntityInWorld(item);
-                for(int i = 0; i < exp; i++) {
-                    worldIn.spawnEntityInWorld(new EntityXPOrb(worldIn, (double) pos.getX() + 0.5D, (double) pos.getY() + 0.5D, (double) pos.getZ() + 0.5D, 1));
-                }
-        }
-        return super.onBlockDestroyed(stack, worldIn, state, pos, entityLiving);
-    }
+        return true;
+    }*/
 
+    @Override
+    public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ) {
+        Random r = new Random();
+        int i = r.nextInt(10);
+        double d = r.nextDouble();
+        int x = pos.getX();
+        int y = pos.getY() + 1;
+        int z = pos.getZ();
+        double speed = 0.1D;
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 0.0D, (double) y + 0.0D, (double) z + 0.0D , speed, speed, speed);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 0.0D, (double) y + 0.0D, (double) z + 1.0D , speed, speed, -speed);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 1.0D, (double) y + 0.0D, (double) z + 0.0D , -speed, speed, speed);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 1.0D, (double) y + 0.0D, (double) z + 1.0D , -speed, speed, -speed);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 0.0D, (double) y + 1.0D, (double) z + 0.0D , speed, -speed, speed);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 0.0D, (double) y + 1.0D, (double) z + 1.0D , speed, -speed, -speed);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 1.0D, (double) y + 1.0D, (double) z + 0.0D , -speed, -speed, speed);
+        worldIn.spawnParticle(EnumParticleTypes.FLAME, (double) x + 1.0D, (double) y + 1.0D, (double) z + 1.0D , -speed, -speed, -speed);
+        return super.onItemUse(stack, playerIn, worldIn, pos, hand, facing, hitX, hitY, hitZ);
+    }
 }
